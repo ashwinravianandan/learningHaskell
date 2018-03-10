cedb335fa80af2379cd725412a38be9f915c4e31
Ashwin Ravi Anandan <ashwin.anandan@gmail.com>
2018-03-10 12:49:08 +0530

    Written in case syntax

--- Main.hs
+++ Main.hs
@@ -1,27 +1,27 @@
 module Main where
-
-fibonaci :: Integer -> Integer
-fibonaci x 
+fibonacci :: Integer -> Integer
+fibonacci x 
   | 0 == x = 1
   | 1 == x = 1
-  | otherwise = fibonaci (x-1) + fibonaci(x-2)
+  | otherwise = fibonacci (x-1) + fibonacci(x-2)
  
-find :: Integer -> [Integer]-> String
-find _ [] = "Not Found"
-find  x (f : fs )
-   | x > f = find x fs
-   | x == f = "Found it"
-   | otherwise = "Not a fib"
+find :: [Integer] -> Integer -> Bool
+find [] _ = False
+find  (f : fs ) x = case compare f x of
+  GT -> False
+  LT -> find fs x
+  EQ -> True
+
+findInFib :: Integer -> Bool
+findInFib = find $ map fibonacci [0..]
 
 main :: IO()
 main = do
   putStrLn "Enter number you want to test for fibonacci:"
-  number <- getLine
-  let 
-    num :: Integer
-    num = read number
-  putStrLn $ find num $ map fibonaci [0..]
-
+  val <-  (findInFib . read) <$> getLine
+  if True == val then
+    putStrLn "The value occurs in the fibonacci series"
+  else putStrLn "This is not a fibonacci number"
 
   -- warp, qfpl, applied-fp-course
   -- hayoo, local-hoogle
